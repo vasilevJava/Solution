261f2c8e6b8021458e7217480cb6e00a226d4455
maksim <vasilevmaksim4794@gmail.com>
2024-07-25 23:27:41 +0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    byte b;
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;
+    boolean g;
+    char c;
+}
